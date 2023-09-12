85476827446eb3d01b1f8ac91c4739e81bbfecbc
Vivek K J <vivekkj2004@gmail.com>
2023-09-12 12:18:41 +0530

    Delete server dir

--- server/routes/userRoute.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import userController from '../controllers/userController.js';
-const router = express.Router(); 
-router.get('/users/getuser', userController.getUsers);
-router.get('/users/registeruser', userController.registerUsers);
-
-export default router; 
