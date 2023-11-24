54c0783a3a7ad4625b3c3a3421bf72413236f878
Vivek K J <vivekkj2004@gmail.com>
2023-11-24 21:39:01 +0530

    Add a hardcoded sample record always

--- client/src/app/my-documents/page.tsx
+++ client/src/app/my-documents/page.tsx
@@ -33,11 +33,9 @@ const MyDocs = () => {
       axios
         .get("/users/getuserdetails/" + user?.refresh_token.identity)
         .then((res) => {
-          axios
-            .get(`/file/document/user/${res.data.user.id}`)
-            .then((res) => {
-              setRecords(res.data);
-            });
+          axios.get(`/file/document/user/${res.data.user.id}`).then((res) => {
+            setRecords(res.data);
+          });
         });
     }
   }, [user]);
@@ -49,9 +47,49 @@ const MyDocs = () => {
           <h2 className="text-3xl font-bold text-primary">My Documents</h2>
         </div>
         <div className="px-8 md:px-16 py-10 bg-white mx-5 md:mx-20 my-10 rounded-2xl h-[75%] overflow-y-scroll scrollbar-hide">
-          {Records.length === 0 && (
+          {/* {Records.length === 0 && (
             <h2 className="text-center font-bold">No Records Found</h2>
-          )}
+          )} */}
+          <div className="py-4 shadow-md px-10 my-10 rounded-lg" key={"xyz123"}>
+            <h1 className="text-xl font-semibold">{}</h1>
+
+            <p>Patient Data after MRI</p>
+            <div className="flex justify-between items-center">
+              <div className="flex gap-2">
+                <Link
+                  className="bg-primary text-white px-4 py-2 rounded-full"
+                  href={
+                    "https://drive.google.com/file/d/1VTIO12_7jibgUj-zhEQKX66BLjyGZ4F4/view?usp=sharing"
+                  }
+                  target="_blank"
+                >
+                  View
+                </Link>
+                <Popup
+                  trigger={
+                    <a className="bg-primary text-white px-4 py-2 rounded-full">
+                      Share
+                    </a>
+                  }
+                  position="bottom center"
+                >
+                  <div>
+                    <QRCode
+                      value={
+                        "https://drive.google.com/file/d/1VTIO12_7jibgUj-zhEQKX66BLjyGZ4F4/view?usp=sharing"
+                      }
+                    />
+                  </div>
+                </Popup>
+              </div>
+              <p className="text-right text-sm">
+                Uploaded by John Doe, XYZ Hospital
+              </p>
+            </div>
+            <p className="text-right text-sm">
+              on {new Date("1700842065293").toDateString()}
+            </p>
+          </div>
           {Records.map((rec) => (
             <div
               className="py-4 shadow-md px-10 my-10 rounded-lg"
@@ -78,13 +116,14 @@ const MyDocs = () => {
                     position="bottom center"
                   >
                     <div>
-
-                    <QRCode
-                      value={rec.pdfFiles
-                        ? `${process?.env?.NEXT_PUBLIC_BACKEND_URL}/file/document/${rec?.pdfFiles[0]}`
-                        : "/"}
+                      <QRCode
+                        value={
+                          rec.pdfFiles
+                            ? `${process?.env?.NEXT_PUBLIC_BACKEND_URL}/file/document/${rec?.pdfFiles[0]}`
+                            : "/"
+                        }
                       />
-                      </div>
+                    </div>
                   </Popup>
                 </div>
                 <p className="text-right text-sm">
